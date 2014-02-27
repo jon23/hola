c157b0743f85b7de69d880926f4e356d57a90b95
jonathan <jon_ernst@hotmail.com>
2014-02-27 09:47:00 -0200

    anda

---
+++ hola_mundo/src/hola_mundo/Hola.java
@@ -0,0 +1,7 @@
+package hola_mundo;
+
+public interface Hola {
+	
+	
+
+}
--- hola_mundo/src/hola_mundo/Hola2.java
+++ hola_mundo/src/hola_mundo/Hola2.java
@@ -3,7 +3,7 @@ package hola_mundo;
 public class Hola2 {
 
 	public static void main(String[] args) {
-		System.out.println("anda");
+		System.out.println("anda ");
 
 	}
 
